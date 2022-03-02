252fba23f707185c0af66216b971cade77e5ca87
franzi1999 <61967851+franzi1999@users.noreply.github.com>
2022-03-02 09:59:36 +0100

    Lernkasten erscheinen

--- public/JavaScript/Lernkasten.js
+++ public/JavaScript/Lernkasten.js
@@ -1,4 +1,5 @@
 class Lernkasten {
+
     get lernkastenElement() {
         return this._lernkastenElement;
     }
@@ -14,19 +15,25 @@ class Lernkasten {
         this._lernkasten = value;
     }
     constructor(lernkasten) {
+
         this._lernkasten = lernkasten;
         this._lernkastenElement = $(".lernkasten");
     }
 
     erstelleAnsicht() {
+        console.info(this._lernkasten);
         for (var i = 0; i <this._lernkasten.length; i++) {
             var newP = document.createElement("p");
-            newP.button({
-                label: this._lernkasten[i],
-                click:function(t){
-
-                }
+            var button =    $(document.createElement('button')).prop({
+                type: 'button',
+                innerHTML: this._lernkasten[i],
+                class: 'ui-button ui-corner-all ui-widget',
+                click: function () { }
             });
+            console.log(button[0]);
+            newP.append(button[0]);
+            console.log(newP);
+            this._lernkastenElement.append(newP);
         }
     }
 }
--- public/controller.php
+++ public/controller.php
@@ -2,22 +2,28 @@
 include 'Model/Lernkarte.php';
 include 'Model/Lernkasten.php';
 
-$verzeichnis='../Lernkasten';
+$verzeichnis='./Lernkasten';
 
 switch ($_GET['action']){
     case 'getLernkaesten':
-        if ( $handle = opendir($verzeichnis) )
-        {
-            // einlesen der Verzeichnisses
-            while (($file = readdir($handle)) !== false)
+        if (is_dir($verzeichnis)){
+            if ( $handle = opendir($verzeichnis) )
             {
-                if ($file !=='.' || $file !=='..'){
-                    $lernkaesten = $file;
+                while (($file = readdir($handle)) !== false)
+                {
+                    if ($file !=='.' || $file !=='..'){
+                        $lernkaesten[] = $file;
+                    }
                 }
+                closedir($handle);
             }
-            closedir($handle);
         }
-        return $lernkaesten;
+        for ($i = 2, $iMax = count($lernkaesten); $i< $iMax; $i++){
+            $return[]= $lernkaesten[$i];
+        }
+
+
+        echo json_encode(['result' => $return]);
         break;
     case 'getLernkasten':
         break;
